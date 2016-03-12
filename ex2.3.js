5b5ec6617b3a8e5e6afb0f8b8adbad0503a23ac7
zoeszhen <zoe.zhenshi@gmail.com>
2016-03-12 17:22:26 +0200

    test version
    test

--- ex2.3.js
+++ ex2.3.js
@@ -6,22 +6,15 @@ http.createServer(function(request, response){
 response.writeHead(200,{"Content-Type":"text/plain", "Access-Control-Allow-Origin":"*"});
     var params = url.parse(request.url,true).query;
     
-    var paraA=params.data1;
-    console.log("data1:"+paraA);
-    if(paraA=="sin(x)")
-      {
-      var sin = [];
-		  var final=0;
-			    for (var i = -3.14; i < 3.14; i+=0.1) {
-			    	final=(i-((i*i*i)/6)+((i*i*i*i*i)/120)-((i*i*i*i*i*i*i)/5040))
-			        sin.push([i.toFixed(2), final.toFixed(2)]);
-			       
-			    }
-	
-    }
+    var i=params.i;
+    console.log("data1:"+i);
+   
+	final=(i-((i*i*i)/6)+((i*i*i*i*i)/120)-((i*i*i*i*i*i*i)/5040))
+			        
     console.log("sucees to push");
-    console.log(sin)
-    response.write(String(sin));
+    console.log(final)
+   
+    response.write(String(final));
 	
 	response.end();
 	
